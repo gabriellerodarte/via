2bc31adc00f8b01530bb20fa23db67fc9c1466ee
Gabrielle <ms.grodarte@gmail.com>
2025-07-02 15:33:27 -0700

    create DB

---
+++ server/migrations/versions/00449ad84b49_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 00449ad84b49
+Revises: 
+Create Date: 2025-07-02 15:31:39.136348
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '00449ad84b49'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
