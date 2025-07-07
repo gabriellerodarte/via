e6373d2985d7eade47b58518fc4470363b60983d
Gabrielle <ms.grodarte@gmail.com>
2025-07-07 14:53:24 -0700

    create marshmallow schemas

---
+++ server/schemas.py
@@ -0,0 +1,31 @@
+from config import ma
+from models import User, Trip, Place, Event
+
+class PlaceSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Place
+        load_instance = True
+
+class EventSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Event
+        load_instance = True
+
+class PlaceWithEventsSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Place
+        load_instance = True
+    events = ma.Nested("EventSchema", many=True)
+
+class TripSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Trip
+        load_instance = True
+    places = ma.Nested("PlaceWithEventsSchema", many=True)
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+        fields = ["id", "username", "trips"]
+    trips = ma.Nested("TripSchema", many=True)
