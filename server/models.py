4d520290b333cea02d65574b7e3ad8f119681c6c
Gabrielle <ms.grodarte@gmail.com>
2025-07-07 13:21:01 -0700

    create trip model

--- server/models.py
+++ server/models.py
@@ -9,3 +9,17 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String, nullable=False)
+
+    trips = db.relationship('Trip', back_populates='user', cascade='all, delete-orphan')
+
+class Trip(db.Model):
+    __tablename__ = "trips"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', back_populates='trips')
