494e2efb245b276636764e0e80bc6c70c0abe5ef
Gabrielle <ms.grodarte@gmail.com>
2025-07-07 18:02:37 -0700

    add event resource

--- server/app.py
+++ server/app.py
@@ -12,10 +12,15 @@ from schemas import UserSchema, TripSchema, PlaceWithEventsSchema, EventSchema, 
 user_schema = UserSchema()
 place_schema = PlaceSchema()
 places_schema = PlaceSchema(many=True)
+event_schema = EventSchema()
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return {'error': 'Unauthorized: Please login'}, 401
 
 class Signup(Resource):
     def post(self):
@@ -58,10 +63,8 @@ class Login(Resource):
             return {'error': 'Invalid username'}, 401
 
 class CheckSession(Resource):
+    @login_required
     def get(self):
-        if not current_user.is_authenticated:
-            return {'error': 'User not logged in'}, 401
-
         return user_schema.dump(current_user), 200
 
 class Logout(Resource):
@@ -70,18 +73,14 @@ class Logout(Resource):
         return {}, 204
 
 class PlaceResource(Resource):
+    @login_required
     def get(self):
-        if not current_user.is_authenticated:
-            return {'error': 'Access denied: You are not logged in'}, 401
-
         try:
             places = Place.query.all()
             return places_schema.dump(places), 200
     
+    @login_required
     def post(self):
-        if not current_user.is_authenticated:
-            return {'error': 'Access denied: You are not logged in'}, 401
-
         data = request.get_json()
         name = data.get('name')
         address = data.get('address')
@@ -99,6 +98,44 @@ class PlaceResource(Resource):
             db.session.rollback()
             return {'error': str(e)}, 500
 
+class EventResource(Resource):
+    @login_required
+    def post(self):
+        data = request.get_json()
+        title = data.get('title')
+        planning_status = data.get('planning_status', 'tentative')
+        start_time = data.get('start_time')
+        end_time = data.get('end_time')
+        trip_id = data.get('trip_id')
+        place_id = data.get('place_id')
+
+        if not title or not planning_status or not trip_id or not place_id:
+            return {'error': 'Title, planning_status, trip_id, and place_id are required'}, 400
+        
+        if planning_status not in ['tentative', 'confirmed']:
+            return {'error': 'Invalid planning_status value'}, 400
+
+        if start_time and end_time and start_time >= end_time:
+            return {'error': 'Start time must be before end time'}, 400
+
+        try:
+            new_event = Event(
+                title=title, 
+                planning_status=planning_status,
+                start_time=start_time,
+                end_time=end_time, 
+                trip_id=trip_id, 
+                place_id=place_id
+            )
+            db.session.add(new_event)
+            db.session.commit()
+
+            return event_schema.dump(new_event), 200
+        except Exception as e:
+            db.session.rollback()
+            return {'error': str(e)}, 500
+            
+
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
@@ -108,6 +145,7 @@ api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(PlaceResource, '/places', endpoint='places')
+api.add_resource(EventResource, '/events', endpoint='events')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
