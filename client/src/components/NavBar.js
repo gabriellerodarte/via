d2640f8b05376a5e83ffcc766568f5909dc8f4db
Gabrielle <ms.grodarte@gmail.com>
2025-07-09 11:41:59 -0700

    style navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react"
 import { NavLink, useNavigate } from "react-router-dom"
 import { UserContext } from "../context/UserContext"
+import "../styles/navbar.css"
 
 
 function NavBar() {
@@ -19,18 +20,18 @@ function NavBar() {
     }
 
     return (
-        <nav>
-            <div>
-                <div>via.</div>
-                <div>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/my-trips">View My Trips</NavLink>
-                    <NavLink to="/places">Browse All Places</NavLink>
+        <nav className="navbar">
+            <div className="navbar-left">
+                <div className="logo">via.</div>
+                <div className="nav-links">
+                    <NavLink to="/" className="nav-link">Home</NavLink>
+                    <NavLink to="/my-trips" className="nav-link">Trips</NavLink>
+                    <NavLink to="/places" className="nav-link">Explore</NavLink>
                 </div>
             </div>
-            <div>
-                <button onClick={handleLogout}>Log Out</button>
-                {error && <div>{error}</div>}
+            <div className="navbar-right">
+                <button onClick={handleLogout} className="logout-button">Log Out</button>
+                {error && <div className="error-message">{error}</div>}
             </div>
         </nav>
     )
