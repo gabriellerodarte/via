345499ddb0721853e9e832e63259a262f27dd73b
Gabrielle <ms.grodarte@gmail.com>
2025-07-08 18:29:53 -0700

    create global place context

---
+++ client/src/context/PlaceContext.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const PlaceContext = React.createContext()
+
+function PlaceProvider({ children }) {
+    const [places, setPlaces] = useState([])
+
+    const getPlaces = async () => {
+        try {
+            const r = await fetch(`/places`, {
+                credentials: 'include'
+            })
+            if (r.ok) {
+                const data = await r.json()
+                setPlaces(data)
+            } else {
+                const errorData = await r.json()
+                return { success: false, error: errorData.error || "Failed to fetch places." }
+            }
+        } catch (err) {
+            console.log("Error fetching places:", err)
+            return { success: false, error: "An unexpected error occurred while fetching places."}
+        }
+    }
+
+    return (
+        <PlaceContext.Provider value={{ places, getPlaces }}>
+            {children}
+        </PlaceContext.Provider>
+    )
+}
+
+export { PlaceContext, PlaceProvider }
